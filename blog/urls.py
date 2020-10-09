4e5109818ed2c2350e2895dd94a7f5c516d59b08
Kevin Gonzalez <kevin17@mesoamericana.edu.gt>
2020-10-09 12:31:19 -0600

    login, borradores, elimiar

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.publicacion_lista, name='publicacion_lista'),
-     path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
-     path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
+    path('publicacion/<int:pk>/', views.publicacion_detalle, name='detalle_publicacion'),
+    path('publicacion/nueva', views.publicacion_nueva, name='publicacion_nueva'),
     path('publicacion/<int:pk>/editar/', views.publicacion_editar, name='publicacion_editar'),
+    path('publicacion/borrador/lista', views.publicacion_borrador_lista, name='publicacion_borrador_lista'),
+    path('publicacion/<int:pk>/publicar/', views.publicacion_publicar, name='publicacion_publicar'),
+    path('publicacion/<int:pk>/eliminar/', views.publicacion_eliminar, name='publicacion_eliminar'),
 ]
